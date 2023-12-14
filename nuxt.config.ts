246245238f9ec4a715d7eb3d6ba6fdc029463353
Eden-Cohen1 <edencohen.dev@gmail.com>
2023-12-14 11:41:03 +0200

    ready for production

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,26 +31,11 @@ export default defineNuxtConfig({
       callback: "/",
       exclude: ["/login/reset"],
     },
-    image: {
-      providers: {
-        random: {
-          provider: "netlify",
-          netlify: {
-            baseURl: "",
-            baseURL: "",
-          },
-        },
-      },
-    },
-    postcss: {
-      plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-        cssnano:
-          process.env.NODE_ENV === "production"
-            ? { preset: ["default", { discardComments: { removeAll: true } }] }
-            : false, // disable cssnano when not in production
-      },
+  },
+  image: {
+    provider: "netlify",
+    netlify: {
+      baseURl: process.env.IMAGES_URL || "",
     },
   },
 });
